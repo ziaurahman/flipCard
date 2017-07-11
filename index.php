6586f9cec61eebe0a1f13c015f1583c189a3a26e
zia <ziaurahman.saqib@gmail.com>
2017-07-11 15:37:12 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="container">
+  <div class="card-deck">
+    <div class="card" style="">
+      <div class="info-card">
+          <div class="front">somethig</div>
+          <div class="back">
+          <h2>Gentlesir</h2>
+          <p> Phosfluorescently actualize flexible methods of empowermentPhosfluorescently actualize flexible methods of empowermentPhosfluorescently actualize flexible methods of empowermentPhosfluorescently actualize flexible methods of empowerment </p>
+          </div>
+      </div>
+    </div>
+
+    <div class="card" style="">
+      <div class="info-card">
+          <div class="front">somethig</div>
+          <div class="back"><h2>Gentlesir</h2><p> Phosfluorescently actualize flexible methods of empowerment </p></div>
+      </div>
+    </div> 
+
+    <div class="card" style="">
+      <div class="info-card">
+          <div class="front">somethig</div>
+          <div class="back"><h2>Gentlesir</h2><p> Phosfluorescently actualize flexible methods of empowerment </p></div>
+      </div>
+    </div>
+
+    <div class="card" style="">
+      <div class="info-card">
+          <div class="front">somethig</div>
+          <div class="back"><h2>Gentlesir</h2><p> Phosfluorescently actualize flexible methods of empowerment </p></div>
+      </div>
+    </div>
+  </div>
+</div>
